b4d29cedef83e679a8691fe225acef75c0c78945
Matheus Muniz <matheusmuniz0810@gmail.com>
2024-08-10 19:53:00 -0300

    feature: Created Sign up Screen | all content inserted

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,9 +1,10 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './components/login/login.component';
+import { SignUpComponent } from './components/sign-up/sign-up.component';
 
 export const routes: Routes = [
     {
         path: "",
-        component: LoginComponent
+        component: SignUpComponent
     }
 ];
